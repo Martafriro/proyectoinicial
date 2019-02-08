a941b4ee2f52b038606d2442c53b072810fd985f
martafriro <friasrodriguezmarta@gmail.com>
2019-02-08 09:05:23 +0100

    Inicio del proyecto

---
+++ src/proyectoinicial/proyectoinicial.java
@@ -0,0 +1,10 @@
+package proyectoinicial;
+
+public class proyectoinicial {
+
+	public static void main(String[] args) {
+		// TODO Apéndice de método generado automáticamente
+//inicio del proyecto
+	}
+
+}
